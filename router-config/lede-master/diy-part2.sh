64d35b0c6c7fec9fa796154ef87e1815d4ce3922
lj8812 <74142076+lj8812@users.noreply.github.com>
2022-07-29 11:02:56 +0800

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -8,6 +8,22 @@
 
 # ------------------------------- Main source started -------------------------------
 #
+sed -i 's/^[ \t]*//g' ./.config
+make defconfig
+# 网络配置信息，将从 zzz-default-settings 文件的第2行开始添加
+sed -i "2i # network config" ./package/lean/default-settings/files/zzz-default-settings
+# 默认 IP 地址，旁路由时不会和主路由的 192.168.1.1 冲突
+sed -i "3i uci set network.lan.ipaddr='192.168.6.128'" ./package/lean/default-settings/files/zzz-default-settings
+sed -i "4i uci set network.lan.proto='static'" ./package/lean/default-settings/files/zzz-default-settings # 静态 IP
+sed -i "5i uci set network.lan.type='bridge'" ./package/lean/default-settings/files/zzz-default-settings  # 接口类型：桥接
+sed -i "6i uci set network.lan.ifname='eth0'" ./package/lean/default-settings/files/zzz-default-settings  # 网络端口：默认 eth0，第一个接口
+sed -i "7i uci set network.lan.netmask='255.255.255.0'" ./package/lean/default-settings/files/zzz-default-settings    # 子网掩码
+sed -i "8i uci set network.lan.gateway='192.168.6.1'" ./package/lean/default-settings/files/zzz-default-settings  # 默认网关地址（主路由 IP）
+sed -i "9i uci set network.lan.dns='192.168.6.1'" ./package/lean/default-settings/files/zzz-default-settings  # 默认上游 DNS 地址
+sed -i "10i uci set dhcp.lan.ignore='1'" ./package/lean/default-settings/files/zzz-default-settings  # 旁路由关闭DHCP功能（去掉uci前面的#生效）
+sed -i "11i uci delete network.lan.type" ./package/lean/default-settings/files/zzz-default-settings  # 旁路由去掉桥接模式（去掉uci前面的#生效）                         
+sed -i "12i uci commit network\n" ./package/lean/default-settings/files/zzz-default-settings
+
 # 5.修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
 
@@ -19,7 +35,7 @@ sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package
 echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/192.168.1.1/192.168.6.128/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.6.128/g' package/base-files/files/bin/config_generate
 
 # Modify default root's password（FROM 'password'[$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.] CHANGE TO 'your password'）
 # sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
